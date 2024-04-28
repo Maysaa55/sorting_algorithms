074bafca259a391f0c48070af99a4edc2831f1e1
Maysaa55 <maysaaalimhmd@gmail.com>
2024-04-28 12:54:38 -0700

    cfsndkj,

--- sort.h
+++ sort.h
@@ -3,8 +3,6 @@
 
 #include <stdlib.h>
 #include <stdio.h>
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -18,6 +16,8 @@ typedef struct listint_s
     struct listint_s *prev;
     struct listint_s *next;
 } listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
