bd931234655babd23a368186a6b39123737fcc46
Maysaa_ali <140776197+Maysaa55@users.noreply.github.com>
2024-05-01 08:19:44 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,17 +10,21 @@
  *
  * Return: No Return
  */
-void bubble_sort(int *array, size_t size){
+void bubble_sort(int *array, size_t size)
+{
 size_t i, j;
 int temp;
-if(size < 2)
+if (size < 2)
 	exit(0);
-for(i = 0; i < size; i++){
-for(j = 0; j < size; j++){
-if(array[j] > array[j+1]){
+for (i = 0; i < size; i++)
+{
+for (j = 0; j < size; j++)
+{
+if (array[j] > array[j + 1])
+{
 temp = array[j];
-array[j] = array[j+1];
-array[j+1] = temp;
+array[j] = array[j + 1];
+array[j + 1] = temp;
 print_array(array, size);
 }
 }
